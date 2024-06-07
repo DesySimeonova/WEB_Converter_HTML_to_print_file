b7e865255367430e1266381935f3898aec4a8da5
DesySimeonova <96577243+DesySimeonova@users.noreply.github.com>
2024-06-07 18:45:35 +0300

    update

--- project/php/register.php
+++ project/php/register.php
@@ -1,23 +1,59 @@
 <?php
-session_start();
-require_once "config.php";
 
-if ($_SERVER["REQUEST_METHOD"] == "POST") {
-    $username = trim($_POST["username"]);
-    $email = trim($_POST["email"]);
-    $password = trim($_POST["password"]);
+require_once("db.php");
 
-    $sql = "INSERT INTO users (username,email, password) VALUES (?, ?, ?)";
-    if ($stmt = $mysqli->prepare($sql)) {
-        $stmt->bind_param("sss", $username, $email, password_hash($password, PASSWORD_DEFAULT));
-        if ($stmt->execute()) {
-            header("location: ../login.html");
-        } else {
-            echo "Something went wrong. Please try again later.";
-        }
-        $stmt->close();
+function isUserDataValid($userData) {
+
+    if (!isset($userData["username"]) && !isset($userData["password"])) {
+        return ["isValid" => false, "message" => "Некоректни данни!"];
     }
-    $mysqli->close();
-}
-?>
 
+    return ["isValid" => true, "message" => "Данните са валидни!"];
+
+}
+
+if ($_POST) {
+
+    $valid = isUserDataValid($_POST);
+
+    if (!$valid["isValid"]) {
+        http_response_code(400);
+        exit($valid["message"]);
+    }
+
+    $username = $_POST["username"];
+    $password = password_hash($_POST["password"], PASSWORD_DEFAULT);
+
+    try {
+
+        $db = new DB();
+        $connection = $db->getConnection();
+
+        $sql = "SELECT * FROM users WHERE username = :username";
+        $stmt = $connection->prepare($sql);
+        $stmt->execute(["username" => $username]);
+
+        if ($stmt->rowCount() > 0) {
+            // Username already exists
+            echo json_encode(["status" => "ERROR", "message" => "Username already taken."]);
+        }
+
+        $sql = "INSERT INTO users (username, password) VALUES (:username, :password)";
+        $stmt = $connection->prepare($sql);
+        $stmt->execute(["username" => $username, "password" => $password]);
+
+        echo json_encode(["status" => "SUCCESS", "message" => "Регистрацията е успешна"]);
+
+    } catch (PDOException $e) {
+        http_response_code(500);
+        echo json_encode(["status" => "ERROR", "message" => "Грешка при регистрация!"]);
+    }
+
+
+} else {
+    http_response_code(400);
+    echo json_encode(["status" => "ERROR", "message" => "Некоректни данни!"]); 
+}
+
+
+?>
