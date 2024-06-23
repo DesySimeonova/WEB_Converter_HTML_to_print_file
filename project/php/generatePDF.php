68157c63bc8ef5840d9a9687b8180a8ea817bd83
DesySimeonova <96577243+DesySimeonova@users.noreply.github.com>
2024-06-23 19:54:14 +0300

    update

--- project/php/generatePDF.php
+++ project/php/generatePDF.php
@@ -10,6 +10,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $showPageNumbers = isset($_POST['pageNumberOption']) && $_POST['pageNumberOption'] === 'show';
     $pageSize = isset($_POST['pageSize']) ? $_POST['pageSize'] : 'Letter';
     $pageOrientation = isset($_POST['pageOrientation']) ? $_POST['pageOrientation'] : 'Portrait';
+    $fontSelection = isset($_POST['fontSelection']) ? $_POST['fontSelection'] : 'Helvetica';
 
     $options = new Options();
     $options->set('isHtml5ParserEnabled', true);
@@ -18,6 +19,15 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
 
     $dompdf = new Dompdf($options);
 
+    //$htmlContent = "<style>body { font-family: '{$fontSelection}'; }</style>" . $htmlContent;
+    $css = "<style>
+    body {
+        font-family: '{$fontSelection}';
+    }
+    </style>";
+
+    // Combine the CSS and the content
+    $htmlContent = $css . "<div>{$htmlContent}</div>";
     // To add logic here to split HTML content into pages based on maxWords
 
 
