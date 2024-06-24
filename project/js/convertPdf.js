4f85d02ad1a9e85620fff3e219c3789488aaab4c
DesySimeonova <96577243+DesySimeonova@users.noreply.github.com>
2024-06-24 19:35:44 +0300

    fixing bug

--- project/js/convertPdf.js
+++ project/js/convertPdf.js
@@ -39,9 +39,33 @@ document.addEventListener('DOMContentLoaded', () => {
         } else {
             alert('Please attach an HTML file.');
         }
+
+        if (file && file.type === 'text/html') {
+            const formData = new FormData();
+            formData.append('file', file);
+    
+            fetch('php/save_history.php', {
+                method: 'POST',
+                body: formData
+            })
+            .then(response => response.json())
+            .then(data => {
+                if (data.status === 'SUCCESS') {
+                    alert('Файлът успешно е записан в базата данни.');
+                    
+                    fileInput.value = '';
+    
+                } else {
+                    alert('Грешка при запис на файл: ' + data.message);
+                }
+            })
+            .catch(error => console.error('Грешка:', error));
+        } else {
+            alert('Моля, прикачете HTML файл.');
+        }
     });
 
-    document.getElementById('pdfForm').addEventListener('submit', function(event) {
+    /*document.getElementById('pdfForm').addEventListener('submit', function(event) {
         event.preventDefault();
     
         const file = fileInput.files[0];
@@ -68,7 +92,7 @@ document.addEventListener('DOMContentLoaded', () => {
         } else {
             alert('Моля, прикачете HTML файл.');
         }
-    });
+    });*/
     
     
 });
