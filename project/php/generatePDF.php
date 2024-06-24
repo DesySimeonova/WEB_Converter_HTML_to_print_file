a68f3322d0096d69b4ef723def503b6ebab5dc05
DesySimeonova <96577243+DesySimeonova@users.noreply.github.com>
2024-06-24 16:17:57 +0300

    add new functionalities

--- project/php/generatePDF.php
+++ project/php/generatePDF.php
@@ -11,6 +11,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $pageSize = isset($_POST['pageSize']) ? $_POST['pageSize'] : 'Letter';
     $pageOrientation = isset($_POST['pageOrientation']) ? $_POST['pageOrientation'] : 'Portrait';
     $fontSelection = isset($_POST['fontSelection']) ? $_POST['fontSelection'] : 'Helvetica';
+    $fontSize = isset($_POST['fontSize']) ? $_POST['fontSize'] : 14;
 
     $options = new Options();
     $options->set('isHtml5ParserEnabled', true);
@@ -23,13 +24,22 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $css = "<style>
     body {
         font-family: '{$fontSelection}';
+        font-size: {$fontSize}pt; 
     }
     </style>";
 
-    // Combine the CSS and the content
+    if ($showPageNumbers) {
+        // Add page numbering script
+        $htmlContent .= '<script type="text/php">
+            if (isset($pdf)) { 
+                $pdf->page_script(\'if ($PAGE_COUNT > 1) {
+                    $pdf->text(270, 800, "Page: $PAGE_NUM of $PAGE_COUNT", "Helvetica", 10);
+                }\');
+            }
+        </script>';
+    }
+   
     $htmlContent = $css . "<div>{$htmlContent}</div>";
-    // To add logic here to split HTML content into pages based on maxWords
-
 
     $dompdf->loadHtml($htmlContent);
     $dompdf->setPaper($pageSize, $pageOrientation);
